10767d0a90605380f27af45b72588722cfb2e103
othmanebe <othmane.bellous@gmail.com>
2023-04-25 14:16:07 +0100

    edit links

---
+++ javascript/file.js
@@ -0,0 +1,19 @@
+
+let productCard = document.getElementsByClassName("product-card"); 
+
+  for (let i = 0; i < productCard.length; i++){
+      productCard[i].addEventListener("click", function () {
+        location.href = "../product.html";
+      });
+    }
+
+    let cart = document.getElementsByClassName("cart"); 
+    cart[0].addEventListener("click", function () {
+      location.href = "../cart.html";
+    });
+
+    let user = document.getElementsByClassName("user"); 
+    user[0].addEventListener("click", function () {
+      location.href = "../account.html";
+    });
+

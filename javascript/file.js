d780cbd8fd543fab916f8dd576eed2235dd7d4f3
othmanebe <othmane.bellous@gmail.com>
2023-04-25 15:27:13 +0100

    links update

--- javascript/file.js
+++ javascript/file.js
@@ -3,17 +3,17 @@ let productCard = document.getElementsByClassName("product-card");
 
   for (let i = 0; i < productCard.length; i++){
       productCard[i].addEventListener("click", function () {
-        location.href = "../product.html";
+        location.href = "product.html";
       });
     }
 
     let cart = document.getElementsByClassName("cart"); 
     cart[0].addEventListener("click", function () {
-      location.href = "../cart.html";
+      location.href = "cart.html";
     });
 
     let user = document.getElementsByClassName("user"); 
     user[0].addEventListener("click", function () {
-      location.href = "../account.html";
+      location.href = "account.html";
     });
 
